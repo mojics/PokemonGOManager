6535a61959e51f0295ebd2dc770dacd3a382fca5
Ronald <ronald@aleph-labs.com>
2016-08-18 19:04:31 +0800

    Add backend files

---
+++ backend/app/controllers/home.js
@@ -0,0 +1,69 @@
+var express = require('express'),
+  router = express.Router(),
+  PokemonGo = require('../models/pokemongo');
+
+module.exports = function (app) {
+  app.use('/', router);
+
+};
+
+router.get('/', function (req, res, next) {
+
+  res.json([]);
+
+});
+
+
+
+router.get('/articles', function (req, res, next) {
+
+  res.json([{'title':"Sample"}]);
+
+});
+
+
+
+
+router.get('/login', function (req, res, next) {
+
+  res.render('login',{
+    'error': req.query.error
+  });
+});
+
+
+
+
+router.post('/pokemons', function (req, res, next) {
+
+  var pokemongo = new PokemonGo(req.body);
+
+
+  //pokemongo.getInventoryDetails(function(details){
+  //  if(details.error === true){
+  //    res.status(500).json(details);
+  //
+  //  }else{
+  //    res.json(details);
+  //  }
+  //});
+
+  pokemongo.getInventoryDetails(function(details){
+    if(details.error === true){
+      req.session = req.session || {};
+
+      req.session = details;
+      //console.log(req.session);
+      res.redirect('/login?error='+details.message);
+    }else{
+      //res.json(details);
+      console.log(details.pokemonlist);
+      res.render('pokemons',{
+        pokemons: details.pokemon,
+        pokemonlist: details.pokemonlist
+      });
+    }
+  });
+
+
+});
